14269f3bd3d22b8be12da98a53c1b26b10165047
NobleLogik <git@noblelogik.com>
2018-01-17 15:19:32 -0500

    Store cards in descending order
    By doing this at init time, it is unnecessary to verify it everytime in
    other methods.

--- src/main/java/Hand.java
+++ src/main/java/Hand.java
@@ -3,20 +3,23 @@ public class Hand{
     private Card c1, c2;
 
     public Hand(Card c1, Card c2){
-        this.c1 = c1;
-        this.c2 = c2;
+        // place cards in descending order
+        Value v1 = c1.getValue();
+        Value v2 = c2.getValue();
+        if(v1.compareTo(v2) < 0){
+            // v2 > v1,
+            this.c1 = c2;
+            this.c2 = c1;
+        }else{
+            // v1 >= v2
+            this.c1 = c1;
+            this.c2 = c2;
+        }
     }
 
     public String toString(){
         Value v1 = this.c1.getValue();
         Value v2 = this.c2.getValue();
-
-        // highest card first
-        if(v1.compareTo(v2) < 0){
-            Value tmp = v1;
-            v1 = v2;
-            v2 = tmp;
-        }
 
         String str = v1.toString() + v2.toString();
 

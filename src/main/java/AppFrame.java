cceee08afbb90947f1936968f693bde8d84fc90c
NobleLogik <git@noblelogik.com>
2018-01-19 19:53:17 -0500

    Move AppFrame responsability to View
    Giving the View access to the application window. This allows the View
    to modify the windows and its contents.

--- src/main/java/AppFrame.java
+++ src/main/java/AppFrame.java
@@ -5,8 +5,6 @@ public class AppFrame extends JFrame{
     public AppFrame(){
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setBounds(0,0,1200,900);
-        this.setContentPane(new SimpleRangeMatrix());
         this.setTitle("PokerPlanner");
-        this.setVisible(true);
     }
 }
--- src/main/java/View.java
+++ src/main/java/View.java
@@ -1,8 +1,19 @@
+import javax.swing.JFrame;
+
 public class View{
 
     private MVC mvc;
+    private AppFrame frame;
 
     public View(MVC mvc){
         this.mvc = mvc;
+        this.frame = new AppFrame();
+        switchToSimpleRange();
+        this.frame.setVisible(true);
     }
+
+    private void switchToSimpleRange(){
+        this.frame.setContentPane(new SimpleRangeMatrix());
+    }
+
 }
